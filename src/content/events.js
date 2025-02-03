ec53172e1e9988f9e2a8f9aeac674375e1e90b0f
abe1242 <abmdn1242@gmail.com>
2025-02-03 21:00:02 +0530

    Fix deadline

--- src/content/events.js
+++ src/content/events.js
@@ -1,25 +1,35 @@
 const eventsDetails = [
-    {
-        id: "IMPACT-X",
-        name : "IMPACT X",   
-        details: "“The power to change the world lies within the ideas we dare to pursue.”\n\nImagine a room filled with energy, visionaries like you turning bold ideas into groundbreaking solutions. The stakes are high, and time is ticking. Will your idea change the world?⚡\n\nAthena 25’, the Annual Technical Fest of ISTE TKMCE, presents IMPACT X, where creativity meets purpose, and bold ideas transform into game-changing solutions. This event challenges you to identify a real-world problem, craft an innovative solution, and make a meaningful impact using technology.🚀\n\n📌 Date      : 15 February, 2025\n📌 Venue   : APJ Hall\n📌 Time     : 10:00 AM\n📌 Reg fee : 200/-\n\n⭕Register at : https://bit.ly/ImpacTX\n\nFor further queries, contact:\nParthiv: 8075451097\nAswin: 7907583776",
-        date : "15 February, 2025",
-        posters: ["/events/ImpacT-X/logo.jpeg","/events/ImpacT-X/guidelines.jpeg"],
-        price : "200 RS",
-        coordinators : [{name: "Parthiv", phone:" 8075451097"},{name: "Aswin", phone:"7907583776"}],
-        link:"https://bit.ly/ImpacTX",
-        deadline : "15 February, 2025"
-    },
-    {
-        id: "Nature's-Nest",
-        name : "Nature's Nest",   
-        details: "A  building designing competition based on biomimicry.",
-        date : "13 - 16 February, 2025",
-        posters: ["/events/Nature's-Nest/logo.jpeg","/events/Nature's-Nest/guidelines.jpeg"],
-        price : "200 RS",
-        coordinators : [{name: "Rabia S", phone:" 7902655814"},{name: "Roshmina", phone:"7012682980"}],
-        link:"https://makemypass.com/natures-nest",
-        deadline : "13 February, 2025"
-    }
-]
+  {
+    id: "IMPACT-X",
+    name: "IMPACT X",
+    details:
+      "“The power to change the world lies within the ideas we dare to pursue.”\n\nImagine a room filled with energy, visionaries like you turning bold ideas into groundbreaking solutions. The stakes are high, and time is ticking. Will your idea change the world?⚡\n\nAthena 25’, the Annual Technical Fest of ISTE TKMCE, presents IMPACT X, where creativity meets purpose, and bold ideas transform into game-changing solutions. This event challenges you to identify a real-world problem, craft an innovative solution, and make a meaningful impact using technology.🚀\n\n📌 Date      : 15 February, 2025\n📌 Venue   : APJ Hall\n📌 Time     : 10:00 AM\n📌 Reg fee : 200/-\n\n⭕Register at : https://bit.ly/ImpacTX\n\nFor further queries, contact:\nParthiv: 8075451097\nAswin: 7907583776",
+    date: "15 February, 2025",
+    posters: ["/events/ImpacT-X/logo.jpeg", "/events/ImpacT-X/guidelines.jpeg"],
+    price: "200 RS",
+    coordinators: [
+      { name: "Parthiv", phone: " 8075451097" },
+      { name: "Aswin", phone: "7907583776" },
+    ],
+    link: "https://bit.ly/ImpacTX",
+    deadline: "15 February, 2025",
+  },
+  {
+    id: "Nature's-Nest",
+    name: "Nature's Nest",
+    details: "A  building designing competition based on biomimicry.",
+    date: "13 - 16 February, 2025",
+    posters: [
+      "/events/Nature's-Nest/logo.jpeg",
+      "/events/Nature's-Nest/guidelines.jpeg",
+    ],
+    price: "200 RS",
+    coordinators: [
+      { name: "Rabia S", phone: " 7902655814" },
+      { name: "Roshmina", phone: "7012682980" },
+    ],
+    link: "https://makemypass.com/natures-nest",
+    deadline: "12 February, 2025",
+  },
+];
 export default eventsDetails;
