5308981f44c4a3d5ff8b489f465f200143da64fe
Aswinjp-max <aswinjp2255@gmail.com>
2025-02-05 21:53:16 +0530

    COM93

--- src/routes/details.jsx
+++ src/routes/details.jsx
@@ -3,102 +3,54 @@ export default function Details(){
     <div>
       <div className="text-center text-xl">Privacy Policy</div>
       <div className="p-5">
-        At ATHENA'24, we prioritize the privacy and security of our users' personal information. This Privacy Policy describes the types of personal information we collect, how we collect it, how we use it, and how we safeguard it.
-
-Information We Collect
-
-For registration and payment purposes, we may collect the following personal information from our users:
-
-1.Name
-2.Email address
-3.Phone number
-4.College name
-5.Payment information (credit card details or other payment information)
-
-How We Collect Information
-
-We collect personal information through online registration forms and payment gateways. Additionally, if users choose to connect their social media accounts with our registration platform, we may collect information from these platforms.
-
-How We Use Information
-
-The collected personal information is used for the following purposes:
-
-1.Register users for event
-2.Process payments for event registrations
-3.Send notifications and updates about events
-4.Send marketing communications about ATHENA'24 or related events
-5.Conduct research and analysis to improve our events and services.
-
-We do not share or sell personal information to third parties for any purpose other than those listed above.
-
-How We Protect Information
-
-We take reasonable measures to protect users' personal information from unauthorized access, use, or disclosure. We use industry-standard encryption methods to protect payment information and limit access to personal information to authorized personnel only.
-
-However, we cannot guarantee the security of user data transmitted over the internet, as no method of transmission or storage is 100% secure. Users acknowledge and agree that they provide personal information at their own risk.
-
-Retention of Information
-
-We retain user information only for as long as necessary to fulfill the purposes for which it was collected or as required by law. When personal information is no longer needed, we securely dispose of it.
-
-User Rights
-
-Users have the right to access, correct, or delete their personal information, as permitted by relevant laws and regulations. To exercise these rights, users can contact us at the email address provided below.
-
-Updates to Privacy Policy
-
-We reserve the right to update this Privacy Policy at any time without prior notice. We encourage users to review this page periodically for any changes.
-
-Contact Us
-
-If you have any questions or concerns about this Privacy Policy, please contact us at:
+        At ATHENA’25, we prioritize the privacy and security of our users' personal information. This Privacy Policy describes the types of personal information we collect, how we collect it, how we use it, and how we safeguard it. Information We Collect For registration and payment purposes, we may collect the following personal information from our users: 1.Name 2.Email address 3.Phone number 4.College name 5.Payment information (credit card details or other payment information) How We Collect Information We collect personal information through online registration forms and payment gateways. Additionally, if users choose to connect their social media accounts with our registration platform, we may collect information from these platforms. How We Use Information The collected personal information is used for the following purposes: 1.Register users for event 2.Process payments for event registrations 3.Send notifications and updates about events 4.Send marketing communications about ATHENA’25 or related events 5.Conduct research and analysis to improve our events and services. We do not share or sell personal information to third parties for any purpose other than those listed above. How We Protect Information We take reasonable measures to protect users' personal information from unauthorized access, use, or disclosure. We use industry-standard encryption methods to protect payment information and limit access to personal information to authorized personnel only. However, we cannot guarantee the security of user data transmitted over the internet, as no method of transmission or storage is 100% secure. Users acknowledge and agree that they provide personal information at their own risk. Retention of Information We retain user information only for as long as necessary to fulfill the purposes for which it was collected or as required by law. When personal information is no longer needed, we securely dispose of it. User Rights Users have the right to access, correct, or delete their personal information, as permitted by relevant laws and regulations. To exercise these rights, users can contact us at the email address provided below. Updates to Privacy Policy We reserve the right to update this Privacy Policy at any time without prior notice. We encourage users to review this page periodically for any changes. Contact Us If you have any questions or concerns about this Privacy Policy, please contact us at:
       </div>
       <div className="text-center text-xl">Terms Of Use</div>
       <div className="p-5">
-        By registering for or participating in ATHENA'24, you agree to adhere to the following terms and conditions:
+        By registering for or participating in ATHENA'25, you agree to adhere to the following terms and conditions:
 
-To take part in ATHENA'24 events, participants must register online and submit the necessary fees. All registration fees are non-refundable, unless the event is canceled or under exceptional circumstances.
+To take part in ATHENA'25 events, participants must register online and submit the necessary fees. All registration fees are non-refundable, unless the event is canceled or under exceptional circumstances.
 
 Eligibility
 
-Participants must meet the eligibility requirements for each event as specified on the ATHENA'24 website. Participants who do not meet the eligibility requirements may be disqualified from the event.
+Participants must meet the eligibility requirements for each event as specified on the ATHENA'25 website. Participants who do not meet the eligibility requirements may be disqualified from the event.
 
 Code Of Conduct
 
-Participants must adhere to a code of conduct, maintaining respectful and professional behavior throughout ATHENA'24 events. Disruptive or inappropriate behavior may lead to disqualification from the event and/or removal from the premises.
+Participants must adhere to a code of conduct, maintaining respectful and professional behavior throughout ATHENA'25 events. Disruptive or inappropriate behavior may lead to disqualification from the event and/or removal from the premises.
 
 Intellectual Property
 
-Participants retain ownership of any intellectual property they create during ATHENA'24 events. However, participants grant ATHENA'24 and its sponsors a non-exclusive, royalty-free license to use, display, and distribute the intellectual property for promotional purposes.
+Participants retain ownership of any intellectual property they create during ATHENA'25 events. However, participants grant ATHENA'25 and its sponsors a non-exclusive, royalty-free license to use, display, and distribute the intellectual property for promotional purposes.
 
 Disclaimer of Liability
 
-ATHENA'24 and its sponsors are not responsible for any personal injury, property damage, or other loss that may occur during its events. Participants assume all risk and liability for their participation in the events.
+ATHENA'25 and its sponsors are not responsible for any personal injury, property damage, or other loss that may occur during its events. Participants assume all risk and liability for their participation in the events.
 
 Force Majeure
 
-ATHENA'24 and its sponsors are not liable for any failure or delay in performing their obligations due to circumstances beyond their control, including but not limited to acts of God, war, terrorism, or natural disasters.
+ATHENA'25 and its sponsors are not liable for any failure or delay in performing their obligations due to circumstances beyond their control, including but not limited to acts of God, war, terrorism, or natural disasters.
 
 Modification or Cancellation of Events
 
-ATHENA'24 and its sponsors reserve the right to modify or cancel any event or activity at any time for any reason, without liability to participants.
+ATHENA'25 and its sponsors reserve the right to modify or cancel any event or activity at any time for any reason, without liability to participants.
 
 Governing Law and Jurisdiction
 
-These terms and conditions are governed by the laws of the jurisdiction where ATHENA'24 is held and are to be interpreted accordingly. Any disputes arising from these terms and conditions will be exclusively resolved by the courts in that jurisdiction.
+These terms and conditions are governed by the laws of the jurisdiction where ATHENA'25 is held and are to be interpreted accordingly. Any disputes arising from these terms and conditions will be exclusively resolved by the courts in that jurisdiction.
 
 Amendments
 
-ATHENA'24 and its sponsors reserve the right to amend these terms and conditions without prior notice. Participants are advised to regularly review the terms and conditions to ensure compliance.
+ATHENA'25 and its sponsors reserve the right to amend these terms and conditions without prior notice. Participants are advised to regularly review the terms and conditions to ensure compliance.
 
-By registering for or participating in ATHENA'24 events, participants acknowledge that they have read, understood, and agreed to abide by these terms and conditions.
+By registering for or participating in ATHENA'25 events, participants acknowledge that they have read, understood, and agreed to abide by these terms and conditions.
       </div>
 
       <div className="text-center text-xl">Refund Policy</div>
       <div className="p-5">
         Refund Policy
 
-We understand that circumstances may arise where participants may need to cancel their registration for ATHENA' 24 events. We have developed the following refund policy to address such situations:
+We understand that circumstances may arise where participants may need to cancel their registration for ATHENA'25 events. We have developed the following refund policy to address such situations:
 
 Faulty Registration
 If there is an error in registration due to technical issues or system errors, participants may request a refund of their registration fees. The refund will be processed within 7 business days of the request.
@@ -113,8 +65,8 @@ Refund Process
 All refund requests must be made by email to the address provided on the ATHENA' 24 website. Refund requests must include the participant's name, registration details, and reason for the request. Refunds will be processed using the original payment method, and may take up to 7 business days to appear in the participant's account.
 
 Amendments
-ATHENA' 24 reserves the right to amend this refund policy at any time without notice. Participants are responsible for reviewing the policy periodically to ensure compliance.
-By registering for and/or participating in ATHENA' 24 events, participants acknowledge that they have read, understood, and agreed to this refund policy
+ATHENA'25 reserves the right to amend this refund policy at any time without notice. Participants are responsible for reviewing the policy periodically to ensure compliance.
+By registering for and/or participating in ATHENA'25 events, participants acknowledge that they have read, understood, and agreed to this refund policy
       </div>
 
     </div>
