a0f9118196e27832de7de14c4bf4ca67e7221f14
Aswinjp-max <aswinjp2255@gmail.com>
2025-02-05 19:06:51 +0530

    com77

--- src/content/talkshow.js
+++ src/content/talkshow.js
@@ -1,6 +1,6 @@
 const talkshowDetails = [
     {
-            id: "Talksection-A25",
+            id: "Talksection",
             name : "Entrepreneurial-mindset : what it takes to succeed",
             details: "",
             date : "15 Feb 2025",
