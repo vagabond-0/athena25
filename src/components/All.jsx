496c690b1c3fd1f5a69cff96510692ca0ab1b556
amalendu123 <220229@tkmce.ac.in>
2025-02-05 21:08:09 +0530

    done

--- src/components/All.jsx
+++ src/components/All.jsx
@@ -35,7 +35,7 @@ const All = () => {
   };
 
   return (
-    <div className='md:min-h-screen flex flex-col md:flex-row gap-10 justify-center items-center p-10 font-poppins'
+    <div className='md:min-h-screen h-screen flex flex-col md:flex-row gap-10 justify-center items-center p-10 font-poppins'
       style={{
         backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)),url(${bgcarnival})`,
         backgroundSize: "cover",
