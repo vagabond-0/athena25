c8d1ea5f9e16bac37dcfc4dd06779e7c96abcb6e
Aswinjp-max <aswinjp2255@gmail.com>
2025-02-06 01:15:49 +0530

    gfdy

--- src/routes/Talkshows.jsx
+++ src/routes/Talkshows.jsx
@@ -9,7 +9,7 @@ const Talkshows = () => {
   
   return (
     <div className="w-screen min-h-screen  p-10 ">
-      <div className="md:text-6xl text-5xl p-4">
+      <div className="sm:text-6xl text-4xl p-4">
         <h1 style={{fontFamily:" Retro Signed"}}>Talk Sessions</h1>
       </div>
       <div className="flex flex-col  justify-center gap-10 rounded-lg items-center pl-4 pr-4 ">
