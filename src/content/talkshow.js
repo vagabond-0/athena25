fb7c872827ed00913c66f87ebe33f452e709ce3b
Aswinjp-max <aswinjp2255@gmail.com>
2025-02-06 00:26:14 +0530

    com56

--- src/content/talkshow.js
+++ src/content/talkshow.js
@@ -2,9 +2,9 @@ const talkshowDetails = [
     {
             id: "Talksession-A25",
             name : "Entrepreneurial-mindset : what it takes to succeed",
-            details: "",
+            details: "Entrepreneurial Mindset: What It Takes to Succeed is a talk session that delves into the essential traits and strategies of successful entrepreneurs. Led by Amith Raman, CEO of Inker Robotic Solutions Pvt. Ltd., this session will offer valuable insights into cultivating the right mindset, overcoming challenges, and turning ideas into reality through real-world experiences.",
             date : "15 Feb 2025",
-            posters: [" "," "],
+            posters: ["/events/Talksession-A25/poster.jpg"],
             deadline: "15/02/25",
             price : "FREE",
             coordinators : [
--- src/content/workshops.js
+++ src/content/workshops.js
@@ -4,9 +4,9 @@ const workshopDetails= [
         name : "3D Printing Workshop With SolidWorks",
         details: "",
         date : "15 Feb 2025",
-        posters: [" "," "],
+        posters: ["/events/3D-PRINTING-ATHENA25/poster.png","/events/3D-PRINTING-ATHENA25/Syllabus.png"],
         deadline : "13 February, 2025",
-        price : "₹500 for Non-ISTE members,₹400 for ISTE members",
+        price : "₹500 for Non-ISTE members \n ₹400 for ISTE members",
         coordinators : [{name: "Fardeen", phone:"7736055843"},{name: "Nafih", phone:"8113920470"}],
         link: "https://makemypass.com/3d-printing-workshop-2",
         isopen:false
@@ -14,11 +14,11 @@ const workshopDetails= [
 {
     id: "Generative-ai",
     name: "Generative ai",
-    details: "“When machines dream, they dream in code.”\n\nCurious about AI❓ \nAthena ‘25—the Annual Technical Fest of ISTE TKMCE presents Generative AI, a hands-on workshop on optimizing and deploying AI models. Explore the fundamentals of Generative AI and real-world applications in a simple, practical way. 💻 \n\nAI sounds complex? Don’t worry—we’re here to make it as easy as scrolling memes! This workshop welcomes all curious minds, guiding you through a journey to unlock the power of AI. 🚀 \n\nDive in, innovate, and be part of the AI revolution!\n\n Date : 16 February, 2025\n\n Time : 10:00 AM\n \nMode : Offline",
+    details: "Dive into the world of Generative AI with this hands-on workshop, focusing on optimizing and deploying AI models. Led by Nandakishor M, the CEO and AI researcher of Convai Innovations, this session will explore AI fundamentals and real-world applications in an engaging and practical way.",
     date: "16 Feb 2025",
     posters: ["/events/Generative-AI/poster.png",
               "/events/Generative-AI/Syllabus.png"],
-    price: "iste members - 300/-  Non iste members -400/-",
+    price: "iste members - 300/-\n\nNon iste members -400/-",
     coordinators: [
       { name: "DEVIKA A K", phone: " 7594827955" },
       { name: "DEVIKA D", phone: "85472 60003" },
