36fc884fdce64de515ba8c744648bfc1985bdfa9
Aswinjp-max <aswinjp2255@gmail.com>
2025-02-05 18:03:14 +0530

    com6

--- src/content/workshops.js
+++ src/content/workshops.js
@@ -1,3 +1,20 @@
 const workshopDetails= [
+    {
+        id: "Re-Action-Rush",
+        name: "Re-Action-Rush",
+        details: "Action rush is a rapid burst of energy and momentum driving swift and decisive actions.",
+        date: "15 feb 2025",
+        posters: ["/events/Re-Action-Rush/logo.jpeg",
+                  "/events/Re-Action-Rush/guidelines.jpeg"],
+        price: "60 / team",
+        coordinators: [
+          { name: "Sherin", phone: "7306564995" },
+          { name: "Renah", phone: "96053 01454" },
+        ],
+        link: "https://makemypass.com/re-action-rush",
+        deadline: "13 Feb 2025",
+        isopen:true
+      },
+      
 ]
 export default workshopDetails;
