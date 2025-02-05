292ebe60be920033da66f7a9529acde2e15e4cd8
Aswinjp-max <aswinjp2255@gmail.com>
2025-02-05 18:32:39 +0530

    COM7

--- src/content/talkshow.js
+++ src/content/talkshow.js
@@ -5,9 +5,9 @@ const talkshowDetails = [
             details: "",
             date : "15 Feb 2025",
             posters: "",
-            deadline: "13 March 2024",
-            price : "This Event Comes Under Delegate Pass",
-            coordinators : [{name: "Anandu", phone:"8547010891"},{name: "Aiswarya", phone:"7510552966"}],
+            deadline: "15/02/25",
+            price : "FREE",
+            coordinators : [{name: "Hadya unais", phone:"9072211772"},{name: "Muhammed salih u k", phone:"8086219319"}],
             link: "https://makemypass.com/entrepreneurial-mindset",
             isopen:true
 },
