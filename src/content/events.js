1ae02303def7743d1d12fa4a16737eaa447eed92
Aswinjp-max <aswinjp2255@gmail.com>
2025-02-06 13:11:42 +0530

    xvd

--- src/content/events.js
+++ src/content/events.js
@@ -3,7 +3,7 @@ const eventsDetails = [
     id: "IMPACT-X",
     name: "IMPACT X",
     details:
-      "“The power to change the world lies within the ideas we dare to pursue.”\n\nImagine a room filled with energy, visionaries like you turning bold ideas into groundbreaking solutions. The stakes are high, and time is ticking. Will your idea change the world?⚡\n\nAthena 25’, the Annual Technical Fest of ISTE TKMCE, presents IMPACT X, where creativity meets purpose, and bold ideas transform into game-changing solutions. This event challenges you to identify a real-world problem, craft an innovative solution, and make a meaningful impact using technology.🚀\n\n📌 Date      : 15 February, 2025\n📌 Venue   : APJ Hall\n📌 Time     : 10:00 AM\n📌 Reg fee : 200/-\n\n⭕Register at : https://bit.ly/ImpacTX\n\nFor further queries, contact:\nParthiv: 8075451097\nAswin: 7907583776",
+      "IMPACT X is an exciting competition where participants tackle real-world problems with innovative, technology driven solutions. Centered on the theme “Inventions & Innovations for Social Impact,” this event brings together creative minds to develop impactful ideas and pitch them to a panel of judges, showcasing their potential to drive meaningful change.",
     date: "15 February, 2025",
     posters: ["/events/ImpacT-X/logo.jpeg", "/events/ImpacT-X/guidelines.jpeg"],
     price: "200 RS",
--- src/content/workshops.js
+++ src/content/workshops.js
@@ -2,7 +2,7 @@ const workshopDetails= [
     {
         id: "3D-PRINTING-ATHENA25",
         name : "3D Printing Workshop With SolidWorks",
-        details: "",
+        details: "The 3D Printing with SolidWorks Workshop is a hands-on session where participants learn to design, model, and print their creations. Guided by Mr. Muhammed Salmun, CEO of Phiscape Robotics, this workshop covers 3D modeling fundamentals with real-world applications. Attendees will explore how 3D printing transforms ideas into prototypes, gaining practical experience in digital design. Whether you're a beginner or an innovator, this is your chance to turn imagination into reality!",
         date : "15 Feb 2025",
         posters: ["/events/3D-PRINTING-ATHENA25/poster.png","/events/3D-PRINTING-ATHENA25/Syllabus.png"],
         deadline : "13 February, 2025",
@@ -14,7 +14,7 @@ const workshopDetails= [
 {
     id: "Generative-ai",
     name: "Generative ai",
-    details: "Dive into the world of Generative AI with this hands-on workshop, focusing on optimizing and deploying AI models. Led by Nandakishor M, the CEO and AI researcher of Convai Innovations, this session will explore AI fundamentals and real-world applications in an engaging and practical way.",
+    details: "Dive into the world of Generative AI with this hands-on workshop, focusing on optimizing and deploying AI models. Led by Mr.Nandakishor M, the CEO and AI researcher of Convai Innovations, this session will explore AI fundamentals and real-world applications in an engaging and practical way.",
     date: "16 Feb 2025",
     posters: ["/events/Generative-AI/poster.png",
               "/events/Generative-AI/Syllabus.png"],
