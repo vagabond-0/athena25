a735f969c52ace3a0179f41f69e6fac2e2fca373
Aswinjp-max <aswinjp2255@gmail.com>
2025-07-10 19:11:15 +0530

    Update workshops.js

--- src/content/workshops.js
+++ src/content/workshops.js
@@ -15,7 +15,7 @@ const workshopDetails= [
     id: "Generative-ai",
     name: "Generative ai",
     details: "Dive into the world of Generative AI with this hands-on workshop, focusing on optimizing and deploying AI models. Led by Mr.Nandakishor M, the CEO and AI researcher of Convai Innovations, this session will explore AI fundamentals and real-world applications in an engaging and practical way.",
-    date: "16 Feb 2025",
+    date: "27 July 2025",
     posters: ["/events/Generative-AI/poster.png",
               "/events/Generative-AI/Syllabus.png"],
     price: "iste members - 300/-\n\nNon iste members -400/-",
@@ -24,8 +24,8 @@ const workshopDetails= [
       { name: "DEVIKA D", phone: "85472 60003" },
     ],
     link: "https://makemypass.com/event/workshop-on-generative-ai-2",
-    deadline: "13 Feb 2025",
-    isopen:false
+    deadline: "25 July 2025",
+    isopen:true
   },
 
 
