69b64f6ab5cff779ec5574ae4164e7ba8bc75dd8
abe1242 <abmdn1242@gmail.com>
2025-07-11 16:40:01 +0530

    Update event Bytebug

--- src/content/events.js
+++ src/content/events.js
@@ -13,12 +13,13 @@ const eventsDetails = [
     ],
     link: "https://makemypass.com/event/impact-x",
     deadline: "22 July, 2025",
-    isopen:true
+    isopen: true,
   },
   {
     id: "SPARKATHON",
     name: "SPARKATHON",
-    details:"SPARKATHON is a dynamic problem-solving challenge that puts your technical skills and strategic thinking to the test. Participants will tackle a series of intense tasks, pushing their limits in logic, innovation, and adaptability. Compete, overcome obstacles, and prove your mastery in this thrilling battle of intellect.",
+    details:
+      "SPARKATHON is a dynamic problem-solving challenge that puts your technical skills and strategic thinking to the test. Participants will tackle a series of intense tasks, pushing their limits in logic, innovation, and adaptability. Compete, overcome obstacles, and prove your mastery in this thrilling battle of intellect.",
     date: "16 FEBRUARY,2025",
     posters: [
       "/events/SPARKATHON/logo.png",
@@ -31,7 +32,7 @@ const eventsDetails = [
     ],
     link: "https://makemypass.com/sparkathon",
     deadline: "14 February, 2025",
-    isopen:false
+    isopen: false,
   },
   {
     id: "Nature's-Nest",
@@ -51,12 +52,13 @@ const eventsDetails = [
     ],
     link: "https://makemypass.com/event/natures-nest-1",
     deadline: "19 July, 2025",
-    isopen:true
+    isopen: true,
   },
   {
     id: "Mechmerize",
     name: "Mechmerize",
-    details: "Mechmerize is a competition where you unleash your creativity as you reimagine overlooked materials into extraordinary designs. Collaborate in teams and put your engineering skills to the test in this hands-on, offline event. Showcase your ingenuity and compete against the best and turn imagination into reality!",
+    details:
+      "Mechmerize is a competition where you unleash your creativity as you reimagine overlooked materials into extraordinary designs. Collaborate in teams and put your engineering skills to the test in this hands-on, offline event. Showcase your ingenuity and compete against the best and turn imagination into reality!",
     date: "15 February, 2025",
     posters: [
       "/events/Mechmerize/logo.jpg",
@@ -69,32 +71,35 @@ const eventsDetails = [
     ],
     link: "https://makemypass.com/mechmerize",
     deadline: "12 February 2025",
-    isopen:false
+    isopen: false,
   },
   {
     id: "ByteBug",
     name: "ByteBug",
-    details: "ByteBug is a high-stakes debugging challenge where logic meets speed. Work in teams to unravel broken code, identify hidden flaws, and implement quick fixes under pressure. Put your problem-solving skills to the test in this intense competition. Overcome the bugs, race against time, and secure your victory!",
-    date: "15 February, 2025",
-    posters: ["/events/ByteBug/logo.jpg",
-              "/events/ByteBug/guidelines.jpg"],
+    details:
+      "ByteBug is a high-stakes debugging challenge where logic meets speed. Work in teams to unravel broken code, identify hidden flaws, and implement quick fixes under pressure. Put your problem-solving skills to the test in this intense competition. Overcome the bugs, race against time, and secure your victory!",
+    date: "26 July, 2025",
+    posters: ["/events/ByteBug/logo.jpg", "/events/ByteBug/guidelines.jpg"],
     price: "150 RS",
     coordinators: [
       { name: "Athila", phone: " 7034551438" },
       { name: "Abhishek", phone: "9496388163" },
     ],
-    link: "https://makemypass.com/bytebug/",
-    deadline: "12 February, 2025",
-    isopen:false
+    link: "https://makemypass.com/event/byte-bug/",
+    deadline: "24 July, 2025",
+    isopen: false,
   },
-  
+
   {
     id: "League-of-Ludo",
     name: "League-of-Ludo",
-    details: "LEAGUE OF LUDO is a life-sized Ludo board competition where players become the game pieces! Each move comes with unexpected challenges, twists, and power-ups. Solve fun tasks, outplay your opponents, and race to victory in this exciting showdown!",
+    details:
+      "LEAGUE OF LUDO is a life-sized Ludo board competition where players become the game pieces! Each move comes with unexpected challenges, twists, and power-ups. Solve fun tasks, outplay your opponents, and race to victory in this exciting showdown!",
     date: "16 Feb 2025",
-    posters: ["/events/League-of-Ludo/logo.jpg",
-              "/events/League-of-Ludo/guidelines.jpg"],
+    posters: [
+      "/events/League-of-Ludo/logo.jpg",
+      "/events/League-of-Ludo/guidelines.jpg",
+    ],
     price: "80 / team",
     coordinators: [
       { name: "Sneha ", phone: "7356492394" },
@@ -102,15 +107,18 @@ const eventsDetails = [
     ],
     link: "https://makemypass.com/league-of-ludo ",
     deadline: "13 Feb 2025",
-    isopen:false
+    isopen: false,
   },
   {
     id: "Re-Action-Rush",
     name: "Re-Action-Rush",
-    details: "Put your skills to the test at Re-Action Rush, an electrifying challenge at ATHENA ‘25—the Annual Technical Fest of ISTE TKMCE. Dive into a high-stakes race where you must solve puzzles, balance reactions, and think fast to complete the challenge before time runs out. Precision, speed, and a sharp mind are your best tools—every second counts, and only the best will prevail.",
+    details:
+      "Put your skills to the test at Re-Action Rush, an electrifying challenge at ATHENA ‘25—the Annual Technical Fest of ISTE TKMCE. Dive into a high-stakes race where you must solve puzzles, balance reactions, and think fast to complete the challenge before time runs out. Precision, speed, and a sharp mind are your best tools—every second counts, and only the best will prevail.",
     date: "15 feb 2025",
-    posters: ["/events/Re-Action-Rush/logo.png",
-              "/events/Re-Action-Rush/guidelines.jpg"],
+    posters: [
+      "/events/Re-Action-Rush/logo.png",
+      "/events/Re-Action-Rush/guidelines.jpg",
+    ],
     price: "60 / team",
     coordinators: [
       { name: "Sherin", phone: "7306564995" },
@@ -118,15 +126,18 @@ const eventsDetails = [
     ],
     link: "https://makemypass.com/event/re-action-rush-1",
     deadline: "14 Feb 2025",
-    isopen:false
+    isopen: false,
   },
   {
     id: "Red-handed",
     name: "Red-Handed",
-    details: "RED HANDED is an exciting murder mystery challenge where teams must follow cryptic clues and piece together evidence to solve a series of chilling crimes. As the investigation unfolds, every detail becomes crucial, and every second counts. Decode the secrets, connect the clues, and expose the truth before the case goes cold.",
+    details:
+      "RED HANDED is an exciting murder mystery challenge where teams must follow cryptic clues and piece together evidence to solve a series of chilling crimes. As the investigation unfolds, every detail becomes crucial, and every second counts. Decode the secrets, connect the clues, and expose the truth before the case goes cold.",
     date: "15 feb 2025",
-    posters: ["/events/Red-Handed/logo.jpg",
-              "/events/Red-Handed/guidelines.jpg"],
+    posters: [
+      "/events/Red-Handed/logo.jpg",
+      "/events/Red-Handed/guidelines.jpg",
+    ],
     price: "50 RS",
     coordinators: [
       { name: "Ramjith M M", phone: "9188145715" },
@@ -134,9 +145,7 @@ const eventsDetails = [
     ],
     link: "https://makemypass.com/red-handed",
     deadline: "13 Feb 2025",
-    isopen:false
+    isopen: false,
   },
-  
-  
 ];
 export default eventsDetails;
